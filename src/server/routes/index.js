ed79afcc6be5b1ff775c80a2be583d5e382618da
JordonHoshor <Jordon.Hoshor@gmail.com>
2016-11-19 12:57:03 -0700

    inital commit

---
+++ src/server/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const indexController = require('../controllers/index');
+
+router.get('/', getPlayers)
+
+
+
+function  (req, res, next) {
+
+});
+
+module.exports = router;
